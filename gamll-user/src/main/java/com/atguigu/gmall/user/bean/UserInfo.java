531b298f4977b703e306b0b59c86f6e0fcb276a8
starwu <starwu@atguigu.com>
2019-01-08 00:16:16 +0800

    def

---
+++ gamll-user/src/main/java/com/atguigu/gmall/user/bean/UserInfo.java
@@ -0,0 +1,118 @@
+package com.atguigu.gmall.user.bean;
+
+public class UserInfo {
+
+    private String id;
+    private String loginName;
+    private String nickName;
+    private String passwd;
+    private String name;
+    private String phoneNum;
+    private String email;
+    private String headImg;
+    private String userLevel;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHeadImg() {
+        return headImg;
+    }
+
+    public void setHeadImg(String headImg) {
+        this.headImg = headImg;
+    }
+
+    public String getUserLevel() {
+        return userLevel;
+    }
+
+    public void setUserLevel(String userLevel) {
+        this.userLevel = userLevel;
+    }
+
+    public UserInfo() {
+    }
+
+    public UserInfo(String id, String loginName, String nickName, String passwd, String name, String phoneNum, String email, String headImg, String userLevel) {
+        super();
+        this.id = id;
+        this.loginName = loginName;
+        this.nickName = nickName;
+        this.passwd = passwd;
+        this.name = name;
+        this.phoneNum = phoneNum;
+        this.email = email;
+        this.headImg = headImg;
+        this.userLevel = userLevel;
+    }
+
+    @Override
+    public String toString() {
+        return "UserInfo{" +
+                "id='" + id + '\'' +
+                ", loginName='" + loginName + '\'' +
+                ", nickName='" + nickName + '\'' +
+                ", passwd='" + passwd + '\'' +
+                ", name='" + name + '\'' +
+                ", phoneNum='" + phoneNum + '\'' +
+                ", email='" + email + '\'' +
+                ", headImg='" + headImg + '\'' +
+                ", userLevel='" + userLevel + '\'' +
+                '}';
+    }
+
+}
